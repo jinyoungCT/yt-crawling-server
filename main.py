b9971abf3303066abd354948bfe3a8a322c14010
dharana77 <dharana7723@gmail.com>
2021-08-11 14:34:21 +0900

    videoapi 추가

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask import request
+from sandworks_yt_crawling import VideoStat
 
 app = Flask(__name__)
 
 
-@app.route('/video')
-def getVideo(vid):
-	data = ""
-	return data
+@app.route('/video', methods=["GET",])
+def getVideo():
+	vid="T7h8O7dpJIg"
+	video = VideoStat(vid)
+	data = video.extract()
+	#print(data)
+	
+	videoInfo = video.videoInfo
+	#print(videoInfo)
+	
+	return videoInfo
 
+
+if __name__ == "__main__":
+	app.run(host="localhost" , port=8080, debug=True)
