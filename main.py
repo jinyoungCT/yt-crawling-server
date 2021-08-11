158bf1834fb900462b41bf6dbd7eb79bff281859
dharana77 <dharana7723@gmail.com>
2021-08-11 19:07:52 +0900

    branch from video

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
 from flask import request
 from sandworks_yt_crawling import Comment
+from sandworks_yt_crawling import VideoStat
 
 app = Flask(__name__)
 
 
 @app.route('/video', methods=["GET",])
+def getVideo():
+	vid= request.args['vid']
+	
+	video = VideoStat(vid)
+	data = video.extract()
+	#print(data)
+	
+	videoInfo = video.videoInfo
+	if videoInfo == None:
+		return render_template("page_not_found.html"), 404
+	#print(videoInfo)
+	
+	return videoInfo
+
+
+@app.route('/comment', methods=["GET",])
 def getComments():
 	vid= request.args['vid']
 	limit=  request.args['limit']
